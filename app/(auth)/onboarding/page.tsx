4866b12c6c2b75eae135deeddd6b7ef9baf99745
John Kim <mkcokr2176@gmail.com>
2023-08-07 21:15:10 -0700

    Update onboarding page

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,3 +1,5 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+
 async function Page() {
   return (
     <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
@@ -5,6 +7,9 @@ async function Page() {
       <p className='mt-3 text-base-regular text-light-2'>
         Complete your profile now, to use Threds.
       </p>
+      <section className='mt-9 bg-dark-2 p-10'>
+        <AccountProfile user={userData} btnTitle='Continue' />
+      </section>
     </main>
   );
 }
