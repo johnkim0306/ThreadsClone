af25cebb53102dde3c1a6791f2c0c55ca88a5a85
John Kim <mkcokr2176@gmail.com>
2023-08-13 20:06:31 -0700

    Implement Profile page

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,6 +6,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 
 import { connectToDB } from "../mongoose";
+import Thread from "../models/thread.model";
 
 export async function fetchUser(userId: string) {
   try {
