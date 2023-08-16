d2bca4d3a74cd1dc8581580fe34c431915f568da
John Kim <mkcokr2176@gmail.com>
2023-08-15 23:43:24 -0700

    Temporary fix the prolbme by commenting out search bar

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -27,7 +27,7 @@ async function Page({
     <section>
       <h1 className='head-text mb-10'>Search</h1>
 
-      <Searchbar routeType='search' />
+      {/* <Searchbar routeType='search' /> */}
 
       <div className='mt-14 flex flex-col gap-9'>
         {result.users.length === 0 ? (
@@ -48,11 +48,11 @@ async function Page({
         )}
       </div>
 
-      <Pagination
+      {/* <Pagination
         path='search'
         pageNumber={searchParams?.page ? +searchParams.page : 1}
         isNext={result.isNext}
-      />
+      /> */}
     </section>
   );
 }
